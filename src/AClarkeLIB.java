338cac107f45a89d60ae7e374472f9b192030e35
Method Madness <aclarke9691@bths.edu>
2018-10-02 14:49:51 -0400

    First update

--- src/AClarkeLIB.java
+++ src/AClarkeLIB.java
@@ -1,2 +1,20 @@
 public class AClarkeLIB {
+    public static boolean isPalindrome(String word)
+    {
+       String backwards =  "";
+       for (int i= word.length()-1; i= 0; i--)
+       {
+           String a = word.substring(i, i+1);
+           backwards= backwards+a;
+       }
+       if (word.equals(backwards) == true)
+       {
+           return true;
+       }
+       else
+       {
+           return false;
+       }
+
+    }
 }
